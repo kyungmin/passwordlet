eb028af32f47fe3ea0529e6b48598f6cecbb7f7d
Kyungmin Kim <kmin905@gmail.com>
2013-11-15 13:10:32 -0800

    Fix login redirect

--- app/assets/javascripts/routers/app_router.js
+++ app/assets/javascripts/routers/app_router.js
@@ -1,26 +1,4 @@
 Passwordlet.Routers.AppRouter = Backbone.Router.extend({
-  route: function (route, name, callback) {
-    if (!_.isRegExp(route)) route = this._routeToRegExp(route);
-    if (!callback) callback = this[name];
-    
-    var that = this;
-    _.wrap(callback, function (cb) {
-      if ($("#current-user-id").text() != "") {
-        cb();
-      } else {
-        window.location.href = "/users/sign_in";
-      }
-    })();
-    
-    Backbone.history.route(route, _.bind(function(fragment) {
-      var args = this._extractParameters(route, fragment);
-      callback && callback.apply(this, args);
-      this.trigger.apply(this, ['route:' + name].concat(args));
-      Backbone.history.trigger('route', this, name, args);
-    }, this));
-    return this;
-  },
-
   routes: {
     "": "home",
     "domains": "index",
@@ -38,36 +16,49 @@ Passwordlet.Routers.AppRouter = Backbone.Router.extend({
         var indexView = new Passwordlet.Views.IndexView({
           collection: domains
         });
-        that._swapView(indexView);        
+        that._swapView(indexView);
       });
     }
   },
 
   index: function() {
-    var that = this;
-    that._fetchDomains(function(domains) {
-      var indexView = new Passwordlet.Views.IndexView({
-        collection: domains
-      });
-      that._swapView(indexView);        
-    });
+    if ($("current-user-id").text() == "") {
+      window.location.href = "/users/sign_in";
+    } else {
+      var that = this;
+      that._fetchDomains(function(domains) {
+        var indexView = new Passwordlet.Views.IndexView({
+          collection: domains
+        });
+        that._swapView(indexView);        
+      });      
+    }
   },
 
   new: function() {
-    var domain = new Passwordlet.Models.Domain();
-    var newView = new Passwordlet.Views.NewView({
-      collection: Passwordlet.domains,
-      model: domain
-    });
-    this._swapView(newView);
+    if ($("current-user-id").text() == "") {
+      window.location.href = "/users/sign_in";
+    } else {
+      var domain = new Passwordlet.Models.Domain();
+      var newView = new Passwordlet.Views.NewView({
+        collection: Passwordlet.domains,
+        model: domain
+      });
+      this._swapView(newView);
+    }
   },
 
   edit: function(id) {
-    var domain = Passwordlet.domains.get(id);    
-    var editView = new Passwordlet.Views.NewView({
-      model: domain
-    });
-    this._swapView(editView);
+    if ($("current-user-id").text() == "") {
+      window.location.href = "/users/sign_in";
+    } else {
+      this._signedIn();
+      var domain = Passwordlet.domains.get(id);    
+      var editView = new Passwordlet.Views.NewView({
+        model: domain
+      });
+      this._swapView(editView);
+    }
   },
 
   _swapView: function (newView) {
